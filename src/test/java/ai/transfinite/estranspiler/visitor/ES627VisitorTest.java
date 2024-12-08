b013fb5ac37ee1a82923e1d277852000ff7e1776
Ronny Mandal <ronnyma@transfinite.io>
2024-12-08 16:00:10 +0100

    Tests running with expected value.

--- src/test/java/ai/transfinite/estranspiler/visitor/ES627VisitorTest.java
+++ src/test/java/ai/transfinite/estranspiler/visitor/ES627VisitorTest.java
@@ -1,6 +1,8 @@
 package ai.transfinite.estranspiler.visitor;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import ai.transfinite.ES627Lexer;
 import ai.transfinite.ES627Parser;
 import org.antlr.v4.runtime.CharStream;
@@ -13,7 +15,7 @@ public class ES627VisitorTest {
 
     @Test
     void visitAssign() {
-         String boolQuery = """
+        String boolQuery = """
              boolQuery()
               .should(
                   nestedQuery("bostedsadresse",
@@ -46,6 +48,11 @@ public class ES627VisitorTest {
                       , ScoreMode.Max)
               );
             """;
+
+        final String expected =
+            """
+                (query (expression (queries (boolQuery boolQuery() . should ( (nestedQuery nestedQuery ( "bostedsadresse" , (boolQuery boolQuery() . should ( (boolQuery boolQuery() . must ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . must ( (termQuery new TermQueryBuilder( "bostedsadresse.vegadresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) ) . should ( (boolQuery boolQuery() . must ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . must ( (termQuery new TermQueryBuilder( "bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) )) ,  ScoreMode.Max) )))) ) . should ( nestedQuery ( "oppholdsadresse" , boolQuery() . should ( boolQuery() . must ( new TermQueryBuilder( "oppholdsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator ) ) . must ( new TermQueryBuilder( "oppholdsadresse.vegadresse.bruksenhetsnummer.keyword" ,  bruksenhet ) ) ) . should ( boolQuery() . must ( new TermQueryBuilder( "oppholdsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator ) ) . must ( new TermQueryBuilder( "oppholdsadresse.matrikkeladresse.bruksenhetsnummer.keyword" ,  bruksenhet ) ) ) ,  ScoreMode.Max ) ) ;)
+                """;
         CharStream input = CharStreams.fromString(boolQuery);
         ES627Lexer lexer = new ES627Lexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -53,12 +60,13 @@ public class ES627VisitorTest {
         ParseTree tree = parser.query();
         ES627Visitor eval = new ES627Visitor();
         eval.visit(tree);
-        System.out.println(tree.toStringTree(parser));
+        final String stringTree = tree.toStringTree(parser);
+        assertThat(stringTree).isEqualToIgnoringWhitespace(expected);
     }
 
     @Test
     void parseQuery2() {
-         String boolQuery = """
+        String boolQuery = """
               boolQuery()
                      .should(
                          nestedQuery("bostedsadresse",
@@ -75,6 +83,10 @@ public class ES627VisitorTest {
                                  )));
             """;
 
+        final String expected =
+            """
+                (query (expression (queries (boolQuery boolQuery() . should ( (nestedQuery nestedQuery ( "bostedsadresse" , (boolQuery boolQuery() . should ( (boolQuery boolQuery() . must ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . should ( (termQuery new TermQueryBuilder( "bostedsadresse.vegadresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) ) . should ( (boolQuery boolQuery() . should ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . must ( (termQuery new TermQueryBuilder( "bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) )) )) )))) ;)
+                """;
         CharStream input = CharStreams.fromString(boolQuery);
         ES627Lexer lexer = new ES627Lexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -82,6 +94,8 @@ public class ES627VisitorTest {
         ParseTree tree = parser.query();
         ES627Visitor eval = new ES627Visitor();
         eval.visit(tree);
-        System.out.println(tree.toStringTree(parser));
+        final String stringTree = tree.toStringTree(parser);
+        assertThat(stringTree).isEqualToIgnoringWhitespace(expected);
+
     }
 }
