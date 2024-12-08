76fbb44420a66f5cdb4fd1cc57a36263c0904916
Ronny Mandal <ronnyma@transfinite.io>
2024-12-08 17:26:20 +0100

    Tests running with expected value.

--- src/test/java/ai/transfinite/estranspiler/visitor/ES627VisitorTest.java
+++ src/test/java/ai/transfinite/estranspiler/visitor/ES627VisitorTest.java
@@ -13,8 +13,20 @@ import org.junit.jupiter.api.Test;
 
 public class ES627VisitorTest {
 
+    private static void parse(final String boolQuery, final String expected) {
+        CharStream input = CharStreams.fromString(boolQuery);
+        ES627Lexer lexer = new ES627Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ES627Parser parser = new ES627Parser(tokens);
+        ParseTree tree = parser.query();
+        ES627Visitor eval = new ES627Visitor();
+        eval.visit(tree);
+        final String stringTree = tree.toStringTree(parser);
+        assertThat(stringTree).isEqualToIgnoringWhitespace(expected);
+    }
+
     @Test
-    void visitAssign() {
+    void parseQuery1() {
         String boolQuery = """
              boolQuery()
               .should(
@@ -29,8 +41,7 @@ public class ES627VisitorTest {
                               boolQuery()
                                   .must(new TermQueryBuilder("bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword", matrikkelidentifikator))
                                   .must(new TermQueryBuilder("bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword", bruksenhet))
-                          )
-                      , ScoreMode.Max)
+                          ))
               )
               .should(
                   nestedQuery("oppholdsadresse",
@@ -43,25 +54,16 @@ public class ES627VisitorTest {
                           .should(
                               boolQuery()
                                   .must(new TermQueryBuilder("oppholdsadresse.adresseIdentifikatorFraMatrikkelen.keyword", matrikkelidentifikator))
-                                  .must(new TermQueryBuilder("oppholdsadresse.matrikkeladresse.bruksenhetsnummer.keyword", bruksenhet))
-                          )
-                      , ScoreMode.Max)
+                                  .must(new TermQueryBuilder("oppholdsadresse.matrikkeladresse.bruksenhetsnummer.keyword", bruksenhet)),
+                          ScoreMode.Max))
               );
             """;
 
         final String expected =
             """
-                (query (expression (queries (boolQuery boolQuery() . should ( (nestedQuery nestedQuery ( "bostedsadresse" , (boolQuery boolQuery() . should ( (boolQuery boolQuery() . must ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . must ( (termQuery new TermQueryBuilder( "bostedsadresse.vegadresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) ) . should ( (boolQuery boolQuery() . must ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . must ( (termQuery new TermQueryBuilder( "bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) )) ,  ScoreMode.Max) )))) ) . should ( nestedQuery ( "oppholdsadresse" , boolQuery() . should ( boolQuery() . must ( new TermQueryBuilder( "oppholdsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator ) ) . must ( new TermQueryBuilder( "oppholdsadresse.vegadresse.bruksenhetsnummer.keyword" ,  bruksenhet ) ) ) . should ( boolQuery() . must ( new TermQueryBuilder( "oppholdsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator ) ) . must ( new TermQueryBuilder( "oppholdsadresse.matrikkeladresse.bruksenhetsnummer.keyword" ,  bruksenhet ) ) ) ,  ScoreMode.Max ) ) ;)
+                (query (expression (queries (boolQuery boolQuery() . (memberFunctions should) ( (queries (nestedQuery nestedQuery ( "bostedsadresse" , (boolQuery boolQuery() . (memberFunctions should) ( (queries (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" , matrikkelidentifikator ))) ) . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.vegadresse.bruksenhetsnummer.keyword" , bruksenhet ))) ))) ) . (memberFunctions should) ( (queries (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" , matrikkelidentifikator ))) ) . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword" , bruksenhet ))) ))) )) ))) ) . (memberFunctions should) ( (queries (nestedQuery nestedQuery ( "oppholdsadresse" , (boolQuery boolQuery() . (memberFunctions should) ( (queries (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "oppholdsadresse.adresseIdentifikatorFraMatrikkelen.keyword" , matrikkelidentifikator ))) ) . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "oppholdsadresse.vegadresse.bruksenhetsnummer.keyword" , bruksenhet ))) ))) ) . (memberFunctions should) ( (queries (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "oppholdsadresse.adresseIdentifikatorFraMatrikkelen.keyword" , matrikkelidentifikator ))) ) . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "oppholdsadresse.matrikkeladresse.bruksenhetsnummer.keyword" , bruksenhet ))) ) , (scoremode ScoreMode.Max))) )) ))) )))) ;)
                 """;
-        CharStream input = CharStreams.fromString(boolQuery);
-        ES627Lexer lexer = new ES627Lexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ES627Parser parser = new ES627Parser(tokens);
-        ParseTree tree = parser.query();
-        ES627Visitor eval = new ES627Visitor();
-        eval.visit(tree);
-        final String stringTree = tree.toStringTree(parser);
-        assertThat(stringTree).isEqualToIgnoringWhitespace(expected);
+        parse(boolQuery, expected);
     }
 
     @Test
@@ -80,22 +82,57 @@ public class ES627VisitorTest {
                                      boolQuery()
                                          .should(new TermQueryBuilder("bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword", matrikkelidentifikator))
                                          .must(new TermQueryBuilder("bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword", bruksenhet))
-                                 )));
+                                 , ScoreMode.Max))
+                                 );
             """;
 
         final String expected =
             """
-                (query (expression (queries (boolQuery boolQuery() . should ( (nestedQuery nestedQuery ( "bostedsadresse" , (boolQuery boolQuery() . should ( (boolQuery boolQuery() . must ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . should ( (termQuery new TermQueryBuilder( "bostedsadresse.vegadresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) ) . should ( (boolQuery boolQuery() . should ( (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" ,  matrikkelidentifikator )) ) . must ( (termQuery new TermQueryBuilder( "bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword" ,  bruksenhet )) )) )) )) )))) ;)
+                (query (expression (queries (boolQuery boolQuery() . (memberFunctions should) ( (queries (nestedQuery nestedQuery ( "bostedsadresse" , (boolQuery boolQuery() . (memberFunctions should) ( (queries (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" , matrikkelidentifikator ))) ) . (memberFunctions should) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.vegadresse.bruksenhetsnummer.keyword" , bruksenhet ))) ))) ) . (memberFunctions should) ( (queries (boolQuery boolQuery() . (memberFunctions should) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.adresseIdentifikatorFraMatrikkelen.keyword" , matrikkelidentifikator ))) ) . (memberFunctions must) ( (queries (termQuery new TermQueryBuilder( "bostedsadresse.matrikkeladresse.bruksenhetsnummer.keyword" , bruksenhet ))) ) , (scoremode ScoreMode.Max))) )) ))) )))) ;)
                 """;
-        CharStream input = CharStreams.fromString(boolQuery);
-        ES627Lexer lexer = new ES627Lexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ES627Parser parser = new ES627Parser(tokens);
-        ParseTree tree = parser.query();
-        ES627Visitor eval = new ES627Visitor();
-        eval.visit(tree);
-        final String stringTree = tree.toStringTree(parser);
-        assertThat(stringTree).isEqualToIgnoringWhitespace(expected);
+        parse(boolQuery, expected);
 
     }
+
+    @Test
+    void parseQuery3() {
+        String boolQuery = """
+             boolQuery()
+                 .must(
+                     nestedQuery("identifikasjonsnummer",
+                         boolQuery()
+                             .must(termQuery("identifikasjonsnummer.type.keyword", "D_NUMMER"))
+                             .must(termQuery("identifikasjonsnummer.status.keyword", Identifikatorstatuskode.I_BRUK.name()))
+                             .must(termQuery("identifikasjonsnummer.erGjeldende", true))
+                             .must(rangeQuery("identifikasjonsnummer.gyldighetstidspunkt").lte("now-5y")),
+                         ScoreMode.Max)
+                 )
+                 .must(
+                     nestedQuery("personstatus",
+                         boolQuery()
+                             .must(termQuery("personstatus.personstatus.keyword", Personstatustype.MIDLERTIDIG.name()))
+                             .must(termQuery("personstatus.erGjeldende", true))
+                             .must(rangeQuery("personstatus.gyldighetstidspunkt").lte("now-5y")),
+                         ScoreMode.Max)
+                 )
+                 .mustNot(
+                     nestedQuery("identitetsgrunnlag",
+                         boolQuery()
+                             .must(termQuery("identitetsgrunnlag.erGjeldende", true))
+                             .must(termQuery("identitetsgrunnlag.legitimasjonskontroll.keyword", Identitetsgrunnlagstatus.KONTROLLERT.name()))
+                             .must(rangeQuery("identitetsgrunnlag.ajourholdstidspunkt").gt("now-6M")),
+                         ScoreMode.Max)
+            
+                 );
+            """;
+
+        final String expected =
+            """
+                (query (expression (queries (boolQuery boolQuery() . (memberFunctions must) ( (queries (nestedQuery nestedQuery ( "identifikasjonsnummer" , (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery termQuery( "identifikasjonsnummer.type.keyword" , "D_NUMMER" ))) ) . (memberFunctions must) ( (queries (termQuery termQuery( "identifikasjonsnummer.status.keyword" , Identifikatorstatuskode . I_BRUK . name () ))) ) . (memberFunctions must) ( (queries (termQuery termQuery( "identifikasjonsnummer.erGjeldende" , (bool true) ))) ) . (memberFunctions must) ( (queries (rangeQuery rangeQuery( "identifikasjonsnummer.gyldighetstidspunkt" ) . (intervals lte) ( "now-5y" ))) ) , (scoremode ScoreMode.Max)) ))) ) . (memberFunctions must) ( (queries (nestedQuery nestedQuery ( "personstatus" , (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery termQuery( "personstatus.personstatus.keyword" , Personstatustype . MIDLERTIDIG . name () ))) ) . (memberFunctions must) ( (queries (termQuery termQuery( "personstatus.erGjeldende" , (bool true) ))) ) . (memberFunctions must) ( (queries (rangeQuery rangeQuery( "personstatus.gyldighetstidspunkt" ) . (intervals lte) ( "now-5y" ))) ) , (scoremode ScoreMode.Max)) ))) ) . (memberFunctions mustNot) ( (queries (nestedQuery nestedQuery ( "identitetsgrunnlag" , (boolQuery boolQuery() . (memberFunctions must) ( (queries (termQuery termQuery( "identitetsgrunnlag.erGjeldende" , (bool true) ))) ) . (memberFunctions must) ( (queries (termQuery termQuery( "identitetsgrunnlag.legitimasjonskontroll.keyword" , Identitetsgrunnlagstatus . KONTROLLERT . name () ))) ) . (memberFunctions must) ( (queries (rangeQuery rangeQuery( "identitetsgrunnlag.ajourholdstidspunkt" ) . (intervals gt) ( "now-6M" ))) ) , (scoremode ScoreMode.Max)) ))) )))) ;)
+                """;
+        parse(boolQuery, expected);
+
+    }
+
+
 }
